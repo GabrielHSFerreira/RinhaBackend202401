8d3380fada218d25c133e7e3efb683a1b60f1c64
Gabriel Henrique da Silva Ferreira <ghenrique_10@hotmail.com>
2024-02-17 16:56:47 -0300

    Warm Up para EF Core e logs com Serilog.

--- src/RinhaBackend202401.WebApi/Program.cs
+++ src/RinhaBackend202401.WebApi/Program.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RinhaBackend202401.WebApi.Contexts;
+using Serilog;
+using System;
 using System.Text.Json;
 
 namespace RinhaBackend202401.WebApi
@@ -14,30 +17,63 @@ namespace RinhaBackend202401.WebApi
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddControllers()
-                .AddJsonOptions(options =>
-                {
-                    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
-                });
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            CriarLoggerGlobal(builder.Environment);
 
-            builder.Services.AddDbContext<RinhaContext>(options =>
-                options.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
-
-            var app = builder.Build();
-
-            if (app.Environment.IsDevelopment())
+            try
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                builder.Services.AddControllers()
+                        .AddJsonOptions(options =>
+                        {
+                            options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
+                        });
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+                builder.Services.AddDbContext<RinhaContext>(options =>
+                    options.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
+                builder.Host.UseSerilog();
+
+                var app = builder.Build();
+
+                EFWarmUp(app);
+
+                app.UseSerilogRequestLogging();
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+                app.UseAuthorization();
+                app.MapControllers();
+                app.Run();
             }
+            catch (Exception ex)
+            {
+                Log.Logger.Fatal(ex, "Application crashed.");
+                throw;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
 
-            app.UseAuthorization();
+        private static void CriarLoggerGlobal(IWebHostEnvironment environment)
+        {
+            var configuration = new LoggerConfiguration();
 
-            app.MapControllers();
+            if (environment.IsProduction())
+                configuration.MinimumLevel.Error();
 
-            app.Run();
+            Log.Logger = configuration
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+
+        private static void EFWarmUp(WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            using var context = scope.ServiceProvider.GetService<RinhaContext>();
+            _ = context!.Model;
         }
     }
 }
