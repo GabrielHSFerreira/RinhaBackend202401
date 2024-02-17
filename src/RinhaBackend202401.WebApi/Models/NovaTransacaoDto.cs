7593e186507bdbbbf3d071043ff65f2f4708608e
Gabriel Henrique da Silva Ferreira <ghenrique_10@hotmail.com>
2024-02-17 16:56:24 -0300

    Validações para nova transação.

--- src/RinhaBackend202401.WebApi/Models/NovaTransacaoDto.cs
+++ src/RinhaBackend202401.WebApi/Models/NovaTransacaoDto.cs
@@ -1,9 +1,19 @@
-﻿namespace RinhaBackend202401.WebApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RinhaBackend202401.WebApi.Models
 {
     public record NovaTransacaoDto
     {
+        [Required]
+        [Range(1, int.MaxValue)]
         public int Valor { get; init; }
+
+        [Required]
+        [AllowedValues('c', 'd')]
         public char Tipo { get; init; }
+
+        [Required]
+        [StringLength(10, MinimumLength = 1)]
         public string? Descricao { get; init; }
     }
 }
